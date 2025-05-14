c753cd66f2249b3c0d4a1b61f68f379c8cab7a87
Celeste <root@ip-172-31-3-17.us-east-2.compute.internal>
2025-05-14 15:01:58 +0000

    Fix website rendering issue

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-//const connectDB = require("./config/db");
-const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-//const queryguardMiddleware = require("./middleware/queryGuardMiddleware");
-const authRoutes = require("./routes/auth");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+const app = express();
 dotenv.config();
-//connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-//app.use(queryguardMiddleware); // Apply QG logger globally
+app.use(bodyParser.json());
 
-app.use("/auth", authRoutes); // Prefix all /auth routes
+// UNCOMMENT TO TEST MIDDLEWARE
+// const queryguardMiddleware = require("./middleware/queryGuardMiddleware");
+// app.use(queryguardMiddleware); // Apply QG logger globally
+
+// Routes
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 app.use("/search", require("./routes/search"));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+// Serve frontend React build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.use(bodyParser.json());
-//app.use(queryguardMiddleware);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
-// Server start
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
